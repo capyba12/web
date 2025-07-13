2778ebc009c1442cc018cd6288081118f2df1bbd
capyba12 <cbara2346@gmail.com>
2025-07-13 20:07:00 +0200

    Add files via upload

---
+++ sites/3YFdotg7yUtYRdr8mN4Ni6/shared-lib.Dnf6zC4H.mjs
@@ -0,0 +1,3 @@
+import{__esmMin as e}from"./rolldown-runtime.BAw5_7tg.mjs";function t(e,t){return{description:`We’re creating a vibrant collection of 4,444 adorable, doodle-style capybaras, ready to make waves on Cardano!
+`,favicon:`https://framerusercontent.com/assets/a3XMu9l8wihalbKenbiNdRbaJeY.png`,robots:`max-image-preview:large`,socialImage:`https://framerusercontent.com/assets/AI7hns31exBrNexsk7Y4W3Q5FNc.png`,title:`Capys - A Cozy Crew of 4,444 Capybaras`}}var n=e(()=>{});export{n as init_siteMetadata,t as metadata};
+//# sourceMappingURL=shared-lib.Dnf6zC4H.mjs.map
